0c7d823bfedc17fc1991c9604759c1f70223cb2c
Daniel C. West <westdc@vcu.edu>
2020-11-12 15:00:50 -0500

    Made new dataset from pybiomed with some new features then added thme to the concat dataset

--- scripts/concatDatasets.py
+++ scripts/concatDatasets.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
 aac_df = pd.read_csv('../datasets/final_amino_acid_result.csv')
+pybiomed_df = pd.read_csv('../datasets/pybiomed_generated_features.csv')
 bioP_df = pd.read_csv('../datasets/training_data.csv', index_col=0)
 
+
 aac_df_no_id = aac_df.drop(columns=['ID'])
+pybiomed_df_drop = pybiomed_df.drop(columns=['Sequence', 'Label'])
 bioP_df_drop = bioP_df.drop(columns=['PROTEIN SEQUENCE', 'Length', 'Instability Index', 'COUNT-A', 'COUNT-C', 'COUNT-D',
                                      'COUNT-E', 'COUNT-F', 'COUNT-G', 'COUNT-H', 'COUNT-I', 'COUNT-K',
                                      'COUNT-L', 'COUNT-M', 'COUNT-N', 'COUNT-P', 'COUNT-Q', 'COUNT-R',
-                                     'COUNT-S', 'COUNT-T', 'COUNT-V', 'COUNT-W', 'COUNT-Y', 'Cysteines Reduced', 'Cystines Residues'])
+                                     'COUNT-S', 'COUNT-T', 'COUNT-V', 'COUNT-W', 'COUNT-Y', 'Cysteines Reduced',
+                                     'Cystines Residues'])
 
 print(aac_df_no_id)
+print(pybiomed_df_drop)
 print(bioP_df_drop)
 
-concat_dataset = pd.concat([bioP_df_drop, aac_df_no_id], axis=1)
+concat_dataset = pd.concat([bioP_df_drop, pybiomed_df_drop], axis=1)
+# concat_dataset = pd.concat([bioP_df_drop, pybiomed_df_drop, aac_df_no_id], axis=1)
 
 print(concat_dataset)
 
--- scripts/isolate_sequences.py
+++ scripts/isolate_sequences.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 # This script strips the classifications from sequence_training.txt
  
-df = pd.read_csv('sequences_training(3).txt')
+df = pd.read_csv('../datasets/sequences_training(3).txt')
 second_column = df.columns[1]
 df = df.drop([second_column], axis=1)
-df.to_csv('just_sequences.txt', index=False)
+df.to_csv('../datasets/just_sequences.txt', index=False)
 
 print(df)
