5909453796228cc60d1e3dbaea0148ab0d2d63dc
Daniel C. West <westdc@vcu.edu>
2020-11-18 07:27:23 -0500

    Added directories for the training and test datasets, also added a directory for the test dataset results from RapidMiner

--- scripts/concatTestDatasets.py
+++ scripts/concatTestDatasets.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-#aac_df = pd.read_csv('../datasets/final_amino_acid_result.csv')
-pybiomed_df = pd.read_csv('../datasets/TestDatasets/generated_features_test.csv')
-bioP_df = pd.read_csv('../datasets/TestDatasets/testing_data.csv', index_col=0)
-physico_chemical_df = pd.read_csv('../datasets/TestDatasets/test_physico_chemical_result.csv')
-combined_csv_df = pd.read_csv('../datasets/TestDatasets/combined_csv.csv')
-# dipeptide_training_df = pd.read_csv('../datasets/dipeptide_training.csv')
-# final_amphiphilic_pseudo_result_df = pd.read_csv('../datasets/final_amphiphilic_pseudo_result.csv')
-# final_pseudo_result_df = pd.read_csv('../datasets/final_pseudo_result.csv')
-# final_quasi_result_df = pd.read_csv('../datasets/final_quasi_result.csv')
+pybiomed_df = pd.read_csv('../datasets/testDatasets/generated_features_test.csv')
+bioP_df = pd.read_csv('../datasets/testDatasets/testing_data.csv', index_col=0)
+physico_chemical_df = pd.read_csv('../datasets/testDatasets/test_physico-chemical_result.csv')
+combined_csv_df = pd.read_csv('../datasets/testDatasets/test_combined_csv.csv')
+dipeptide_training_df = pd.read_csv('../datasets/testDatasets/dipeptide_test.csv')
+final_amphiphilic_pseudo_result_df = pd.read_csv('../datasets/testDatasets/test_amphiphilic_pseudo_result.csv')
+final_pseudo_result_df = pd.read_csv('../datasets/testDatasets/test_pseudo_result.csv')
+final_quasi_result_df = pd.read_csv('../datasets/testDatasets/test_quasi_result.csv')
 
 
 
-#aac_df_drop = aac_df.drop(columns=['ID'])
-pybiomed_df_drop = pybiomed_df.drop(columns=['Label'])
+pybiomed_df_drop = pybiomed_df.drop(columns=['Sequence'])
 bioP_df_drop = bioP_df.drop(columns=['PROTEIN SEQUENCE', 'Length', 'Instability Index', 'COUNT-A', 'COUNT-C', 'COUNT-D',
                                      'COUNT-E', 'COUNT-F', 'COUNT-G', 'COUNT-H', 'COUNT-I', 'COUNT-K',
                                      'COUNT-L', 'COUNT-M', 'COUNT-N', 'COUNT-P', 'COUNT-Q', 'COUNT-R',
@@ -27,7 +25,7 @@ final_pseudo_result_df_drop = final_pseudo_result_df.drop(columns=['ID'])
 final_quasi_result_df_drop = final_quasi_result_df.drop(columns=['ID'])
 
 #print(aac_df_drop)
-#print(pybiomed_df_drop)
+print(pybiomed_df_drop)
 #print(physico_chemical_df_drop)
 #print(bioP_df_drop)
 #print(combined_csv_df_drop)
@@ -37,10 +35,10 @@ final_quasi_result_df_drop = final_quasi_result_df.drop(columns=['ID'])
 #print(final_quasi_result_df_drop)
 
 concat_dataset_test = pd.concat([bioP_df_drop, pybiomed_df_drop, physico_chemical_df_drop, combined_csv_df_drop,
-                            dipeptide_training_df_drop, final_amphiphilic_pseudo_result_df_drop, final_pseudo_result_df_drop, final_quasi_result_df_drop], axis=1)
-# concat_dataset = pd.concat([bioP_df_drop, pybiomed_df_drop, aac_df_drop], axis=1)
+                            dipeptide_training_df_drop, final_amphiphilic_pseudo_result_df_drop,
+                            final_pseudo_result_df_drop, final_quasi_result_df_drop], axis=1)
 
 print(concat_dataset_test)
 
-concat_dataset_test.to_csv('../datasets/TestDatasets/concat_dataset_test.csv', index=False)
+concat_dataset_test.to_csv('../datasets/testDatasets/concat_dataset_test.csv', index=False)
 
--- scripts/concatTrainingDatasets.py
+++ scripts/concatTrainingDatasets.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-#aac_df = pd.read_csv('../datasets/final_amino_acid_result.csv')
-pybiomed_df = pd.read_csv('../datasets/pybiomed_generated_features.csv')
-bioP_df = pd.read_csv('../datasets/training_data.csv', index_col=0)
-physico_chemical_df = pd.read_csv('../datasets/final_physico_chemical_result.csv')
-combined_csv_df = pd.read_csv('../datasets/bioTriangle_CTD_COMP/combined_csv.csv')
-dipeptide_training_df = pd.read_csv('../datasets/dipeptide_training.csv')
-final_amphiphilic_pseudo_result_df = pd.read_csv('../datasets/final_amphiphilic_pseudo_result.csv')
-final_pseudo_result_df = pd.read_csv('../datasets/final_pseudo_result.csv')
-final_quasi_result_df = pd.read_csv('../datasets/final_quasi_result.csv')
+pybiomed_df = pd.read_csv('../datasets/trainingDatasets/pybiomed_generated_features.csv')
+bioP_df = pd.read_csv('../datasets/trainingDatasets/training_data.csv', index_col=0)
+physico_chemical_df = pd.read_csv('../datasets/trainingDatasets/final_physico_chemical_result.csv')
+combined_csv_df = pd.read_csv('../datasets/trainingDatasets/combined_csv.csv')
+dipeptide_training_df = pd.read_csv('../datasets/trainingDatasets/dipeptide_training.csv')
+final_amphiphilic_pseudo_result_df = pd.read_csv('../datasets/trainingDatasets/final_amphiphilic_pseudo_result.csv')
+final_pseudo_result_df = pd.read_csv('../datasets/trainingDatasets/final_pseudo_result.csv')
+final_quasi_result_df = pd.read_csv('../datasets/trainingDatasets/final_quasi_result.csv')
 
 
 
-#aac_df_drop = aac_df.drop(columns=['ID'])
-pybiomed_df_drop = pybiomed_df.drop(columns=['Label'])
+pybiomed_df_drop = pybiomed_df.drop(columns=['Label', 'Sequence'])
 bioP_df_drop = bioP_df.drop(columns=['PROTEIN SEQUENCE', 'Length', 'Instability Index', 'COUNT-A', 'COUNT-C', 'COUNT-D',
                                      'COUNT-E', 'COUNT-F', 'COUNT-G', 'COUNT-H', 'COUNT-I', 'COUNT-K',
                                      'COUNT-L', 'COUNT-M', 'COUNT-N', 'COUNT-P', 'COUNT-Q', 'COUNT-R',
@@ -37,10 +35,10 @@ print(pybiomed_df_drop)
 #print(final_quasi_result_df_drop)
 
 concat_dataset = pd.concat([bioP_df_drop, pybiomed_df_drop, physico_chemical_df_drop, combined_csv_df_drop,
-                            dipeptide_training_df_drop, final_amphiphilic_pseudo_result_df_drop, final_pseudo_result_df_drop, final_quasi_result_df_drop], axis=1)
-# concat_dataset = pd.concat([bioP_df_drop, pybiomed_df_drop, aac_df_drop], axis=1)
+                            dipeptide_training_df_drop, final_amphiphilic_pseudo_result_df_drop,
+                            final_pseudo_result_df_drop, final_quasi_result_df_drop], axis=1)
 
 print(concat_dataset)
 
-concat_dataset.to_csv('../datasets/concat_dataset.csv', index=False)
+concat_dataset.to_csv('../datasets/trainingDatasets/concat_dataset.csv', index=False)
 
